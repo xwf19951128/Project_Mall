b9d435c16b41e34fc788d17cf41d5b7e5161468c
liza8023 <@126.com>
2019-08-16 11:33:29 +0800

    商场逆向工程

---
+++ src/main/java/com/cskaoyan/mapper/mall/KeywordMapper.java
@@ -0,0 +1,31 @@
+package com.cskaoyan.mapper.mall;
+
+import com.reckon.bean.Keyword;
+import com.reckon.bean.KeywordExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface KeywordMapper {
+    long countByExample(KeywordExample example);
+
+    int deleteByExample(KeywordExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Keyword record);
+
+    int insertSelective(Keyword record);
+
+    List<Keyword> selectByExample(KeywordExample example);
+
+    Keyword selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Keyword record, @Param("example") KeywordExample example);
+
+    int updateByExample(@Param("record") Keyword record, @Param("example") KeywordExample example);
+
+    int updateByPrimaryKeySelective(Keyword record);
+
+    int updateByPrimaryKey(Keyword record);
+}
