e9b8fef6366cbcf33974464fbe6d26070968208d
xwf19951128 <872814505@qq.com>
2019-08-16 16:08:21 +0800

    后两个模块

--- src/main/java/com/cskaoyan/controller/statistic/StatisticController.java
+++ src/main/java/com/cskaoyan/controller/statistic/StatisticController.java
@@ -1,11 +1,22 @@
 package com.cskaoyan.controller.statistic;
 
+import com.cskaoyan.util.ResponseVo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin/stat")
 public class StatisticController {
-//    @RequestMapping("/stat/user")
-//    @RequestMapping("/stat/order")
-//    @RequestMapping("/stat/goods")
+    @RequestMapping("/user")
+    public ResponseVo user(){
+        return null;
+    }
+    @RequestMapping("/order")
+    public ResponseVo order(){
+        return null;
+    }
+    @RequestMapping("/goods")
+    public ResponseVo goods(){
+        return null;
+    }
 }
---
+++ src/main/java/com/cskaoyan/mapper/statistic/StatisticMapper.java
@@ -0,0 +1,4 @@
+package com.cskaoyan.mapper.statistic;
+
+public class StatisticMapper {
+}
