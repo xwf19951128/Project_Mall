fa1a7be4947e12d103c171bbdd05e1809c465302
xiaolei19960829 <1974392131@qq.com>
2019-08-16 17:42:03 +0800

    提交login

--- src/main/java/com/cskaoyan/controller/LoginController.java
+++ src/main/java/com/cskaoyan/controller/LoginController.java
@@ -1,8 +1,9 @@
 package com.cskaoyan.controller;
 
 import com.cskaoyan.bean.vo.Data;
-import com.cskaoyan.bean.vo.ResponseVo2;
-import com.cskaoyan.bean.vo.ResponseVo;
+
+import com.cskaoyan.utils.ResponseVo;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,27 +19,28 @@ public class LoginController {
 
     @RequestMapping("/admin/auth/login")
     @ResponseBody
-    public ResponseVo Login(){
+    public ResponseVo<String> Login(){
         ResponseVo responseVo = new ResponseVo();
         responseVo.setData("c4f33a09-62f4-458e-9f8e-d234bbb8d728");
         responseVo.setErrmsg("成功");
         responseVo.setErrno(0);
         return responseVo;
     }
+
     @RequestMapping("/admin/auth/info")
     @ResponseBody
-    public ResponseVo2 Info(){
+    public ResponseVo<Data> Info(){
         Data data = new Data();
-        ResponseVo2 responseVo2 = new ResponseVo2();
+        ResponseVo responseVo = new ResponseVo();
         String[] roles = {"超级管理员"};
         String[] perms = {"*"};
         data.setRoles(roles);
         data.setName("admin123");
         data.setPerms(perms);
         data.setAvatar("https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
-        responseVo2.setData(data);
-        responseVo2.setErrmsg("成功");
-        responseVo2.setErrno(0);
-        return responseVo2;
+        responseVo.setData(data);
+        responseVo.setErrmsg("成功");
+        responseVo.setErrno(0);
+        return responseVo;
     }
 }
