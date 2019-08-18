eb289a86aa55344a3a0fc89ece006d430258c9b0
liza8023 <@126.com>
2019-08-18 12:05:45 +0800

    interim

--- src/main/java/com/cskaoyan/mapper/mall/StoragesMapper.java
+++ src/main/java/com/cskaoyan/mapper/mall/StoragesMapper.java
@@ -3,6 +3,6 @@ package com.cskaoyan.mapper.mall;
 import com.cskaoyan.bean.mall.file.Storage;
 import org.apache.ibatis.annotations.Param;
 
-public interface StorageMapper {
+public interface StoragesMapper {
     void uploadPic(@Param("storage") Storage storage);
 }
