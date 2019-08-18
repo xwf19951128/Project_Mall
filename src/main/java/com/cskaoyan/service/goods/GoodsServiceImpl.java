2db6daf18dc70d5d3a1842ce1a04a70f437a93a2
tangxuan20141402 <912841857@qq.com>
2019-08-18 13:57:12 +0800

    tx-准备pull

--- src/main/java/com/cskaoyan/service/goods/GoodsServiceImpl.java
+++ src/main/java/com/cskaoyan/service/goods/GoodsServiceImpl.java
@@ -24,7 +24,7 @@ public class GoodsServiceImpl implements GoodsService {
     }
 
     @Override
-    public List<Goods> listPageGoods(PageParams4Goods pageParams4Goods, String goodsSn, String name) {
+    public List<Goods> listPageGoods(PageParams4Goods pageParams4Goods) {
 /*        GoodsExample goodsExample = new GoodsExample();
         PageHelper.startPage(pageParams4Goods.getPage(), pageParams4Goods.getLimit());
         goodsExample.setOrderByClause(pageParams4Goods.getSort() + " " + pageParams4Goods.getOrder());
--- src/test/java/com/cskaoyan/MallApplicationTests.java
+++ src/test/java/com/cskaoyan/MallApplicationTests.java
@@ -1,6 +1,5 @@
 package com.cskaoyan;
 
-import com.cskaoyan.bean.vo.Data;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
